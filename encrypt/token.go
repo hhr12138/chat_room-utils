d0998ddaee9ee505cbbdd36429c3a6ae8fbcb401
houhaoran <1448191711@qq.com>
2022-12-14 13:39:18 +0800

    feat: token生成

--- encrypt/token.go
+++ encrypt/token.go
@@ -1,1 +1,13 @@
 package encrypt
+
+import (
+	"crypto/md5"
+	"strconv"
+)
+
+func Token(username int64, passwd string) string {
+	hash := md5.New()
+	hash.Write([]byte(strconv.FormatInt(username, 16) + passwd))
+	sum := hash.Sum(nil)
+	return string(sum)
+}
