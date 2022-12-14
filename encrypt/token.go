d62cd8600e847cfec51ce9564901af60e27080b0
houhaoran <1448191711@qq.com>
2022-12-14 18:03:07 +0800

    feat: token乱码问题解决, 加上单测

--- encrypt/token.go
+++ encrypt/token.go
@@ -2,6 +2,7 @@ package encrypt
 
 import (
 	"crypto/md5"
+	"fmt"
 	"strconv"
 )
 
@@ -9,5 +10,5 @@ func Token(username int64, passwd string) string {
 	hash := md5.New()
 	hash.Write([]byte(strconv.FormatInt(username, 16) + passwd))
 	sum := hash.Sum(nil)
-	return string(sum)
+	return fmt.Sprintf("%x", sum)
 }
