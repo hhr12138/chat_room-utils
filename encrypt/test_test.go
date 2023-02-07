38a445bbf1a777118a5c78e256edb5666738ff4b
houhaoran <1448191711@qq.com>
2023-02-07 16:36:20 +0800

    feat: 一个并不完善的小工具, 反射实现结构体字段sql.NullXXX->XXX

--- encrypt/test_test.go
+++ encrypt/test_test.go
@@ -2,42 +2,8 @@ package encrypt
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 )
-
-type NullString struct {
-	String string
-	Valid  bool
-}
-
-type Test struct {
-	Name NullString
-}
-
-type Test1 struct {
-	Name string
-}
-
-type User struct {
-	Id     int64
-	Name   string
-	Weight float64
-	Height float64
-}
-
-func TestSqlObjToObj(t *testing.T) {
-	test := &Test{
-		Name: NullString{
-			String: "zhangsan",
-			Valid:  true,
-		},
-	}
-	test1 := new(Test1)
-	reflect.ValueOf(test1).Elem().FieldByName("Name").SetString("haha")
-	SqlObjToObj(test, test1)
-	fmt.Println(test1)
-}
 
 func TestEncryptPassword(t *testing.T) {
 	password := EncryptPassword("hhr12138", "qwer")
